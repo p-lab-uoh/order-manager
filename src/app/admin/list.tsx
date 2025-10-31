8a59a0b42751c01c774c409672f68de48c6b48a2
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2025-11-01 06:14:27 +0900

    feat: update

--- src/app/admin/list.tsx
+++ src/app/admin/list.tsx
@@ -34,7 +34,7 @@ export default function OrderList({
   if (error) return <div>Error loading orders</div>;
 
   return (
-    <div className="grid grid-cols-3 gap-2 m-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 m-2">
       {data.map((order) => (
         <OrderCard key={order.id} order={order} isAdmin />
       ))}
